f9b2dad9e4d46d21a45844b5e21fecb971c742e8
Patricio Zavolinsky <pzavolinsky@yahoo.com.ar>
2020-03-01 02:18:04 -0300

    5

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { createElement } from './dom';
 
-const App = () =>
-  createElement(
-    'div',
-    null,
-    createElement('h1', null, 'This is the title'),
-
-    createElement(
-      'h2',
-      { style: 'background: #ffaaaa' },
-      'This is the subtitle',
-    ),
-  );
+const App = () => (
+  <div>
+    <h1>This is the title</h1>
+    <h2 style="background: #ffaaaa">This is the subtitle</h2>
+  </div>
+);
 
 export default App;
